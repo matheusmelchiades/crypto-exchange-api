c6bd841f7eb06a42e515860f5fc86e7058c6815e
matheusmelchiades <matheusmacielmelchiades@hotmail.com>
2022-07-11 02:40:25 -0300

    add validation in payload

--- src/dto/create-exchange.dto.ts
+++ src/dto/create-exchange.dto.ts
@@ -1,9 +1,34 @@
 import { ExchangeType } from '../interfaces/exchange.interface';
 
+import {
+  IsNumber,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+  IsOptional,
+} from 'class-validator';
+
 export class CreateExchangeDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(5)
+  @Matches('^[A-Z]*$')
   currencyFrom: string;
+
+  @IsNumber()
   amountFrom: number;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(5)
+  @Matches('^[A-Z]*$')
   currencyTo: string;
+
+  @IsNumber()
   amountTo: number;
+
+  @IsString()
+  @IsOptional()
   type?: ExchangeType;
 }
